13149c41588a4c12dcaf21d8e8fffe64d39901b0
DeadHitmann <deadhitman95@gmail.com>
2020-08-02 19:26:40 +0300

    Заливка задания 3

--- src/lessons_java_one/lesson1/Lesson3.java
+++ src/lessons_java_one/lesson1/Lesson3.java
@@ -1,4 +1,145 @@
 package lessons_java_one.lesson1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Lesson3 {
+
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = 'O';
+    private static final char DOT_EMPTY = '.';
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RANDOM = new Random();
+    private static int fieldSizeY;
+    private static int fieldSizeX;
+    private static char[][] field;
+
+    private static void initField() {
+        fieldSizeX = 3;
+        fieldSizeY = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
+    private static void printField() {
+        System.out.print("+");
+        for (int x = 0; x < fieldSizeX * 2 + 1; x++)
+            System.out.print((x % 2 == 0) ? "-" : x / 2 + 1);
+        System.out.println();
+
+        for (int y = 0; y < fieldSizeY; y++) {
+            System.out.print(y + 1 + "|");
+            for (int x = 0; x < fieldSizeX; x++)
+                System.out.print(field[y][x] + "|");
+            System.out.println();
+        }
+
+        for (int x = 0; x <= fieldSizeX * 2 + 1; x++)
+            System.out.print("-");
+        System.out.println();
+    }
+
+    private static void humanTurn() {
+        int x;
+        int y;
+        do {
+            System.out.println("¬ведите координаты хода X и Y (от 1 до " + fieldSizeY + ") через пробел >>> ");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!isValidCell(x, y) || !isEmptyCell(x, y));
+        field[y][x] = DOT_HUMAN;
+    }
+
+    private static boolean isEmptyCell(int x, int y) {
+        return field[y][x] == DOT_EMPTY;
+    }
+
+    private static boolean isValidCell(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    private static void aiTurn() {
+        int x;
+        int y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isEmptyCell(x, y));
+        field[y][x] = DOT_AI;
+    }
+
+    private static boolean checkDraw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (isEmptyCell(x, y)) return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *  2 ѕеределать проверку победы, чтобы она не была реализована просто набором условий.
+     * @param c
+     * @return
+     */
+    private static boolean checkWin(char c) {
+        boolean returnX, returnY, returnD1 = true, returnD2 = true;
+
+        for (int y = 0; y < field.length; y++) {
+            returnX = true;
+            returnY = true;
+
+            for (int x = 0; x < field[y].length; x++) {
+                if (field[y][x] != c)
+                    returnX = false;
+                if (field[x][y] != c)
+                    returnY = false;
+            }
+            if (returnX || returnY)
+                return true;
+            if (field[y][y] != c)
+                returnD1 = false;
+            if (field[y][field.length - 1 - y] != c)
+                returnD2 = false;
+        }
+
+        return returnD1 || returnD2;
+
+
+    }
+
+    public static void main(String[] args) {
+        String answer;
+        do {
+            initField();
+            printField();
+
+            while (true) {
+                humanTurn();
+                if (checkEndGame(DOT_HUMAN, "Human win!")) break;
+                aiTurn();
+                if (checkEndGame(DOT_AI, "Computer win!")) break;
+            }
+            System.out.println("Wanna play again? (y/n) >>> ");
+            answer = SCANNER.next();
+        } while (answer.equals("y"));
+        SCANNER.close();
+    }
+
+    private static boolean checkEndGame(char dot, String winMessage) {
+        printField();
+        if (checkWin(dot)) {
+            System.out.println(winMessage);
+            return true;
+        }
+        if (checkDraw()) {
+            System.out.println("Draw!");
+            return true;
+        }
+        return false;
+    }
 }
